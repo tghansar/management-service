dabda2c0793d973781498283d867fe806a41a7bb
Taariq Ghansar <tghansar@gmail.com>
2023-07-24 01:24:19 +0200

    Modified to H2 Database Setup (Finished)

--- src/main/java/com/example/managementservice/controllers/BookRESTController.java
+++ src/main/java/com/example/managementservice/controllers/BookRESTController.java
@@ -19,7 +19,6 @@ import java.util.*;
 @XmlRootElement(name = "books")
 @Path("/books")
 public class BookRESTController {
-    private static final Map<Long, Book> DB = new HashMap<>();
 
     private final BookRepository bookRepository;
 
@@ -27,21 +26,13 @@ public class BookRESTController {
         this.bookRepository = bookRepository;
     }
 
-//    @GET
-//    @Produces("application/json")
-//    public String getAllBooks(Model model) {
-//        model.addAttribute("books", bookRepository.findAll());
-//        return "books/list";
-//    }
-
     @GET
     @Produces("application/json")
     public Books getAllBooks() {
-//        List<Book> bookList = bookRepository.findAll();
-
         Books books = new Books();
         books.setBooks(new ArrayList<Book>(
                 (Collection<? extends Book>) bookRepository.findAll()));
+
         return books;
     }
 
@@ -57,12 +48,13 @@ public class BookRESTController {
                 || book.getBookType() == null) {
             return Response.status(400).entity("Please provide all inputs").build();
         }
-        book.setId(Long.valueOf(DB.values().size() + 1));
+
+        book.setId(bookRepository.findAll().spliterator().getExactSizeIfKnown() + 1);
         book.setUri("/book-management/" + book.getId());
-//        DB.put(book.getId(), book);
 
         // Save to H2 DB
         bookRepository.save(book);
+
         return Response.status(201).contentLocation(new URI(book.getUri())).build();
     }
 
@@ -90,8 +82,14 @@ public class BookRESTController {
     @Consumes("application/json")
     @Produces("application/json")
     public Response updateBook(@PathParam("id") Long id, Book book) {
-        Book temp = DB.get(id);
-        if (book == null) {
+
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        Book temp;
+
+        if (optionalBook.isPresent()) {
+            temp = optionalBook.get();
+        }
+        else {
             return Response.status(404).build();
         }
 
@@ -101,20 +99,23 @@ public class BookRESTController {
         temp.setPrice(book.getPrice());
         temp.setBookType(book.getBookType());
 
-        DB.put(temp.getId(), temp);
+        bookRepository.save(temp);
+
         return Response.status(200).entity(temp).build();
     }
 
     @DELETE
     @Path("/{id}")
     public Response deleteBook(@PathParam("id") Long id) {
-        Book book = DB.get(id);
 
-        if (book != null) {
-            DB.remove(book.getId());
+        Optional<Book> optionalBook = bookRepository.findById(id);
+
+        if (optionalBook.isPresent()) {
+            bookRepository.deleteById(id);
             return Response.status(200).build();
         }
-
-        return Response.status(404).build();
+        else {
+            return Response.status(404).build();
+        }
     }
 }
